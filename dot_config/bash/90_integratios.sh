8841e4b08aa002a9cdbd4656e3581c82f6e32fb5
kennycallado <kennycallado@hotmail.com>
2023-10-13 19:26:20 +0200

    UPDATE integrations off by default

--- dot_config/bash/90_integratios.sh
+++ dot_config/bash/90_integratios.sh
@@ -12,18 +12,16 @@
 # fi
 
 ## Kitty
-# BEGIN_KITTY_SHELL_INTEGRATION
-if test -n "$KITTY_INSTALLATION_DIR" -a -e "$KITTY_INSTALLATION_DIR/shell-integration/bash/kitty.bash"; then source "$KITTY_INSTALLATION_DIR/shell-integration/bash/kitty.bash"; fi
-# END_KITTY_SHELL_INTEGRATION
+# if test -n "$KITTY_INSTALLATION_DIR" -a -e "$KITTY_INSTALLATION_DIR/shell-integration/bash/kitty.bash"; then source "$KITTY_INSTALLATION_DIR/shell-integration/bash/kitty.bash"; fi
 
 ## Chezmoi
-if [ -n "$(chezmoi --version 2>/dev/null)" ]; then
-  source <(chezmoi completion bash)
-fi
+# if [ -n "$(chezmoi --version 2>/dev/null)" ]; then
+#   source <(chezmoi completion bash)
+# fi
 
 ## Ng
 # check if the ng command exists
-if [ -n "$(ng version 2>/dev/null)" ]; then
+# if [ -n "$(ng version 2>/dev/null)" ]; then
 #   # Load Angular CLI autocompletion.
-  source <(ng completion script)
-fi
+#   source <(ng completion script)
+# fi
